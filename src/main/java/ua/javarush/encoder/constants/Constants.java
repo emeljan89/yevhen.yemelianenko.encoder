c67b65ace8ee717a98e80c93e9548a73f884b84a
Yevhen Yemelianenko <emeljan1989@gmail.com>
2023-08-21 14:46:03 +0300

    create ArrayList "alphabet"

---
+++ src/main/java/ua/javarush/encoder/constants/Constants.java
@@ -0,0 +1,15 @@
+package ua.javarush.encoder.constants;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Constants {
+
+    public static final ArrayList <Character> ALPHABET_EN = new ArrayList<>(
+            Arrays.asList(
+                    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+                    'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+                    'u', 'v', 'w', 'x', 'y', 'z'
+            )
+    );
+}
